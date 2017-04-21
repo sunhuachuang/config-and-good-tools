0bcddb1d410603f066a7f983ac40a9f55d274f8a
sun <huachuang20@gmail.com>
2017-04-21 11:48:10 +0800

    add some setup in install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ pacman -S sudo
 # base
 sudo pacman -S gcc make cmake
 sudo pacman -S yaourt
-sudo pacman -S emacs
 sudo pacman -S nginx
 sudo pacman -S openssl
 sudo pacman -S git
+sudo pacman -S wget
 
 # lang
 sudo pacman -S python
@@ -16,11 +16,21 @@ sudo pacman -S php
 sudo pacman -S composer
 sudo pacman -S ruby
 
+# x-windows
+
 # tool
 sudo pacman -S xsel
 sudo pacman -S ripgrep
+sudo pacman -S proxychains-ng
+sudo pacman -S shadowsocks-qt5
+
+# oh-my-zsh
+sudo pacman -S zsh
+sudo chsh -s $(which zsh)
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # aur
+sudo yaourt -Syu
 sudo yaourt -S xsv
 sudo yaourt -S wrk
 # sudo yaourt -S skypeforlinux-bin
@@ -31,7 +41,8 @@ sudo yaourt -S wrk
 sudo pip install -r requirements.txt
 
 # emacs
-git@github.com:sunhuachuang/emacs.d.git ~/.emacs.d
+sudo pacman -S emacs
+git clone git@github.com:sunhuachuang/emacs.d.git ~/.emacs.d
 
 # rust
 # racer rustfmt
